c103bf8dfe991ad6aae884690e6dfb32a0e13bf1
PalermoD <palermo.deschamps@gmail.com>
2016-02-24 18:56:54 -0500

    changed defult text

--- index.ios.js
+++ index.ios.js
@@ -16,14 +16,7 @@ class VotesterApp extends Component {
     return (
       <View style={styles.container}>
         <Text style={styles.welcome}>
-          Welcome to React Native!
-        </Text>
-        <Text style={styles.instructions}>
-          To get started, edit index.ios.js
-        </Text>
-        <Text style={styles.instructions}>
-          Press Cmd+R to reload,{'\n'}
-          Cmd+D or shake for dev menu
+          Welcome to Votester!
         </Text>
       </View>
     );
